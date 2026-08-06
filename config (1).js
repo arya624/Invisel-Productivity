ac3e5f705656e3133dbbe425e316b92dc8c9d6b7
arya624 <arya@invisel.in>
2026-08-06 12:56:15 +0530

    Update config (1).js

--- config (1).js
+++ config (1).js
@@ -1,5 +1,3 @@
-// Paste your Apps Script Web App URL here after deploying Code.gs.
-// See README.md → "Step 2: Deploy the backend" for how to get this.
 const APP_CONFIG = {
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby1SFWXHb76hvdeXfpmLowPjTFZ525X2VFP0vxBP1YSUCXGfs73FdcdO6fFSCT1i2fOpg/exec"
 };
