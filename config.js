71749701576079155dfe9fdc6c3db5c5ed7b328e
arya624 <arya@invisel.in>
2026-08-06 12:54:20 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Paste your Apps Script Web App URL here after deploying Code.gs.
-// See README.md → "Step 2: Deploy the backend" for how to get this.
-const APP_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwSLQ7zmhjcEjiYsCpPiZy3gSYpvSLpZNnBIbPG9QWw1fvnNqg1UM8USKfVJ2mH5HDLFA/exec"
-};
